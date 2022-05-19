c532ec0f23d4c705a2ec1ad1eeb74e1c3d71dc35
Paul Tarvydas <guitarvydas@gmail.com>
2022-05-19 15:48:43 -0400

    cleanup

--- svgmd.js
+++ svgmd.js
@@ -4,19 +4,15 @@ const grammar = Ohm.grammar (String.raw`
 svgmd {
 Drawing (Drawing) = Network
 
-Network (Network) = Shape ArrowShape*
-ArrowShape = arrow Shape
+Network (Network) = Shape ArrowAndShape*
+ArrowAndShape = arrow Shape
 
 Shape (Shape) = Circle | Box
-Box (Box) = "❲" WordsOrNetwork "❳"
-Circle (Circle) = "❨" WordsOrNetwork "❩"
+Box (Box) = "❲" Phrase "❳"
+Circle (Circle) = "❨" Phrase "❩"
 
 arrow (arrow) = "⟾"
-WordsOrNetwork = 
-  | Network
-  | phrase
-
-phrase = char+
+Phrase = char+
 separator (separator) = "❲" | "❳" | "❨" | "❩" | arrow
 char = ~separator any
 }
@@ -34,6 +30,19 @@ function patternMatch (phrase) {
     }
 }
 
+function stringWidth (s) {
+    return s.length * 12; // stub in 12px for now
+}
+
+function stringHeight (s) {
+    return 12;            // stub in 12px for now
+}
+
+function xLeftMargin () { return 5; }
+function xRightMargin () { return 5; }
+function yTopMargin () { return 5; }
+function yBottomMargin () { return 5; }
+
 const rewriteRules = {
     Drawing : function (_network) {
 	var network = _network.rewrite ();
@@ -45,19 +54,21 @@ const rewriteRules = {
 	return `${s1}${srest}`;
     },
     Shape : function (_Shape) { return _Shape.rewrite (); },
-    Box : function (_lb, _wn, _rb) { 
-	var 
-	var slen = 
-	return `${_lb.rewrite ()}${_wn.rewrite ()}${_rb.rewrite ()}`; 
+	// <rect x="200" y="0" width="120" height="60" fill="#eeffffff" stroke="#000000" pointer-events="all"/>
+    Box : function (_lb, _t, _rb) { 
+	var t = _t.rewrite ({});
+	var previousBB = env.cursor; // cursor is a bounding box (of the previous object) {l, t, r, b}
+	var width = xLeftMargin () + stringWidth (t) + xRightMargin ();
+	var height = yTopMargin () + fontHeight (t) + yBottomMargin ();
+	var newBB = { previousBB.r + xOffset, previousBB.t + yOffset, previousBB.r + width, previousBB.t + height };
+	var svg = `"<rect x="${newBB.l}" y="${newBB.t}" width="${width}" height="${height}" fill="#eeffffff" stroke="#000000" pointer-events="all"/>`;
+	return [svg, newBB];
     },
-    Circle : function (_lb, _wn, _rb) { return `${_lb.rewrite ()}${_wn.rewrite ()}${_rb.rewrite ()}`; },
-    ArrowShape : function (_firstShape,_rest) { return `${_firstShape.rewrite ()}${_rest.rewrite ()}`; },
+    Circle : function (_lb, _t, _rb) { return ["", {}]; },
+    ArrowAndShape : function (_arrow, _shape) { return _shape.rewrite ({}); },
     arrow : function (_k) { return _k.rewrite (); },
 
-    WordsOrNetwork : function (_wn) { return _wn.rewrite (); },
-
-
-    phrase: function (_cPlus) { return _cPlus.rewrite ().join (''); },
+    Phrase: function (_cPlus) { return _cPlus.rewrite ().join (''); },
     separator : function (_k) { return _k.rewrite (); },
     char : function (_k) { return _k.rewrite (); },
     _terminal: function () { return this.sourceString; },
